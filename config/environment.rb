f08cd2f2deeb2ab26e17dfe4a7d89271ca5f8e7c
Dev <dev@Devs-MacBook-Pro.local>
2023-08-04 19:20:59 +0500

    Action Mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.smtp_settings={
+  address: 'smtp.gmail.com',
+  port: 456,
+  domain: 'gmail.com',
+  username: ENV['usama.shahzad@recursolabs.com'],
+  password: ENV['UsamaShahzad24789'],
+  authentication: 'plain',
+  :ssl => true,
+  :tsl => true,
+  enable_sharttls_auto: true
+}
