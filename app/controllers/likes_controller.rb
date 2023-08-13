9b65d91a684d8c13f23311457b6ba095dbce4e6b
UsamaShahzad24789 <usama.shahzad@recursolabs.com>
2023-08-13 19:28:45 +0500

    Fixed minor like functionality Bug

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,12 +6,14 @@ class LikesController < ApplicationController
   end
 
   def new
-    if !Like.find_by(email: current_account.email)
+    debugger
+    current_like=Like.where(post_id:params[:post_id]).where(email:current_account.email)
+    if !(current_like.exists?) 
       @like = Like.new(post_id: params[:post_id], email: current_account.email)
       Like.increment_like(params[:post_id])
       redirect_to root_path if @like.save
     else
-      redirect_to root_path
+      redirect_to root_path      
     end
   end
 end
